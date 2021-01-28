5cb36e2efa4c8cdd7f2160893117e63dbe6f26f8
ajcudd <ajcuddeback@gmail.com>
2021-01-28 09:59:54 -0500

    created post route for income

--- routes/api/income.js
+++ routes/api/income.js
@@ -5,15 +5,27 @@ router.get('/', (req, res) => {
     Income.findAll({
 
     })
-        .then(incomeData => {
-            if (!incomeData) {
+        .then(dbIncomeData => {
+            if (!dbIncomeData) {
                 res.status(404).json({ message: 'There is no income data' })
             }
 
-            res.json(incomeData);
+            res.json(dbIncomeData);
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err)
         })
 });
+
+router.post('/', (req, res) => {
+    Income.create({
+        name: req.body.name,
+        amount: req.body.amount
+    })
+        .then(dbIncomeData => res.json(dbIncomeData))
+        .catch(err => {
+            console.log(err.message);
+            res.status(500).json(err)
+        })
+})
