563da072dfcb652988d925a77a8e0216622b62ec
ajcudd <ajcuddeback@gmail.com>
2021-03-02 14:47:46 -0500

    added month functionality

--- public/assets/javascript/month.js
+++ public/assets/javascript/month.js
@@ -1,29 +1,47 @@
-let nav = 0;
-
 function load() {
+
+    let month;
+    let year;
+
+    month = JSON.parse(localStorage.getItem("month"));
+    year = JSON.parse(localStorage.getItem("year"));
+
     const dt = new Date();
 
-    if (nav !== 0) {
-        dt.setMonth(new Date().getMonth() + nav)
+    if (!month) {
+        localStorage.setItem('month', JSON.stringify(dt.toLocaleDateString('en-us', { month: 'long' })));
+        localStorage.setItem('counter', JSON.stringify(0))
+    }
+    if (!year) {
+        localStorage.setItem('year', JSON.stringify(dt.getFullYear()))
     }
 
-    const year = dt.getFullYear();
-
-    document.querySelector('.month-and-year').innerHTML =
-        `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`
-}
-
-function initButtons() {
-    document.querySelector('.back').addEventListener('click', () => {
-        nav--;
-        load();
-    });
 
     document.querySelector('.next').addEventListener('click', () => {
-        nav++;
-        load();
+        let counter = JSON.parse(localStorage.getItem('counter'))
+        counter = counter + 1;
+        dt.setMonth(new Date().getMonth() + counter)
+        localStorage.clear();
+        localStorage.setItem('counter', JSON.stringify(counter))
+        localStorage.setItem('month', JSON.stringify(dt.toLocaleDateString('en-us', { month: 'long' })))
+        localStorage.setItem('year', JSON.stringify(dt.getFullYear()))
+        document.location.reload()
     })
+
+    document.querySelector('.back').addEventListener('click', () => {
+        let counter = JSON.parse(localStorage.getItem('counter'))
+        counter = counter - 1;
+        dt.setMonth(new Date().getMonth() + counter)
+        localStorage.clear();
+        localStorage.setItem('counter', JSON.stringify(counter))
+        localStorage.setItem('month', JSON.stringify(dt.toLocaleDateString('en-us', { month: 'long' })))
+        localStorage.setItem('year', JSON.stringify(dt.getFullYear()))
+        document.location.reload()
+    })
+
+    document.querySelector('.month-and-year').innerHTML =
+        `${month} ${year}`
 }
 
-initButtons()
+
 load()
